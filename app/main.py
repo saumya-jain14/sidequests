64b091b2cae8f243551d3b9ffe62b0134a3669ab
Saumya <saumya.jain@crunchr.com>
2026-06-30 16:42:59 +0100

    fix

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from .routers import quests, tags
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Side Quests API", version="1.0.0")
+app = FastAPI(title="Side Quests API", version="1.0.0", redirect_slashes=False)
 
 app.add_middleware(
     CORSMiddleware,
